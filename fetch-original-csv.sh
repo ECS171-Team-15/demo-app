5066701a449e606a553d5547343c877dc5985081
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2021-11-20 21:16:16 -0800

    add script to download original.csv dataset

---
+++ fetch-original-csv.sh
@@ -0,0 +1,10 @@
+# Downloads original.csv
+
+#!/usr/bin/env bash
+
+wget https://github.com/ECS171-Team-15/Preprocessed-Dataset/raw/master/original.zip
+unzip original.zip
+
+# Cleanup
+rm -f original.zip
+
