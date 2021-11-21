f337ba3ae0d5232fca8a50333f45a048eeeb4d00
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2021-11-21 13:13:34 -0800

    Upload image instead of dataset

--- app.py
+++ app.py
@@ -9,14 +9,9 @@ import random
 from keras.models import load_model
 import tensorflow as tf
 
-# Button callback function
-def predict_random_ct():
-    # 746 images in total for original dataset
-    st.session_state.image_index = random.randrange(0, 746)
-
-# Convert label int to string output
-def label_str(class_label):
-    if class_label == 1:
+# Convert prediction to string output
+def prediction_str(prediction):
+    if prediction >= 0.5:
         return 'Positive'
     else:
         return 'Negative'
@@ -24,48 +19,38 @@ def label_str(class_label):
 def main():
     st.success("# COVID-19 CT scan classification")
 
-    if 'image_index' not in st.session_state:
-        # Initialize image to use at session start
-        st.session_state.image_index = 0
-
     # Load image
-    "Loading image..."
-    dataset = read_csv('original.csv', nrows=1, skiprows=st.session_state.image_index)
-    # Remove class label
-    class_label = dataset.iloc[0, -1]
-    dataset = dataset.iloc[:, :-1]
+    image_file = st.file_uploader(label="Upload your CT-scan here...", type=['jpg', 'png'])
 
-    # Min-max scaler
-    "Scaling image pixels..."
-    dataset.applymap(lambda x: x / 255)
+    if image_file is None:
+        # No image to classify
+        return
 
-    # Convert dataframe to list of pixels
-    image_pixels = dataset.iloc[0]
+    # Preprocess image on the fly
+    # Convert to grayscale
+    image = Image.open(image_file)
+    image = image.convert("L")
+    # Resize to mean dimensions
+    mean_width = 425
+    mean_height = 302
+    image = image.resize((mean_width, mean_height))
+    # Scale data
+    pixels = list(image.getdata())
+    pixels = [pixel / 255 for pixel in pixels]
 
-    "Loading model..."
     model = load_model('Falsel1_l2.h5')
-
-    # Convert image pixels to Image representation
-    width = 425
-    height = 302
-    image = Image.new('L', (width, height))
-    image.putdata(image_pixels)
 
     # Reshape into 4D tensor
     # Height goes first because its the number of rows
-    dataset = tf.reshape(dataset, (1, height, width, 1))
+    pixels = tf.reshape(pixels, (1, mean_height, mean_width, 1))
 
-    # Model prediction
-    "Making prediction..."
-    predictions = model.predict(dataset)
+    # Get prediction
+    predictions = model.predict(pixels)
     prediction = predictions[0]
 
-    st.image(image, caption="Image " + str(st.session_state.image_index), use_column_width=True)
-
-    "Actual: " + label_str(class_label)
-    "Prediction: " + label_str(prediction)
-
-    st.sidebar.button('Predict Random CT Scan', on_click=predict_random_ct)
+    # Output
+    st.image(image, use_column_width=True)
+    "Prediction: " + prediction_str(prediction)
 
 if __name__=='__main__':
     main()
