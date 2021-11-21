7c98361803254c31e4d8e73dcc0d13b482048a5d
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2021-11-20 21:31:33 -0800

    Remove trailing whitespaces

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import streamlit as st 
+import streamlit as st
 from PIL import Image
 
-def main (): 
+def main ():
     """ All code goes here  """
     st.success("# COVID-19 CT scan classification")
     uploaded_file=st.file_uploader("Upload your scan here...",type=['jpg','png'])
 
-    if uploaded_file is not None: 
+    if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded CT scan', use_column_width=True)
-    
 
 if __name__=='__main__':
     main()
